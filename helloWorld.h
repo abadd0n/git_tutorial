77e78a0c2927362f566925b160b1a5930fed6fea
Anton Komissarov <akomissarov@zedat.fu-berlin.de>
2016-03-29 17:58:53 +0200

    count letters in hello world

--- helloWorld.h
+++ helloWorld.h
@@ -1,8 +1,15 @@
 #include <iostream>
- 
+#include <string>
+
 int main()
 {
+  std::string hw = "Hello World!";
   std::cout << "Hello World!" << std::endl;
-  std::cout << "Bye!" << std::endl;
+  //std::cout << "Bye!" << std::endl;
+  int i = 0;
+  for ( char & c : hw ) {
+	  i++;
+  }
+  std::cout << hw << "has " << i << " characters" << std::endl;
   return 0;
 }
